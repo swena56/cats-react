cf8824fbeb2fa032cf6f66ad98c85de15b84223f
Andrew Swenson <swena56@gmail.com>
2018-03-28 01:15:49 -0600

    cards and slide show

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { Container, Row, Col } from 'reactstrap';
 import CatCard from './Components/CatCard';
+import CatsSlideShow from './Components/CatsSlideShow';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+  }
+
   render() {
+
     return (
       <div className="App">
         Cats
-        <CatCard name="CatName" />
+        <Container>
+          <Row>
+            <Col><CatCard  name="CatName" /></Col>
+            <Col><CatCard name="CatName" /></Col>
+            <Col><CatCard name="CatName" /></Col>
+          </Row>
+          <br/>
+          <Row>
+            <Col><CatsSlideShow /></Col>
+          </Row>
+          <br/>
+        </Container>
+
       </div>
     );
   }
--- src/Components/CatCard.js
+++ src/Components/CatCard.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+import CatPopUp from './CatPopUp';
+
 export default class CatCard extends Component {
 
     constructor(props) {
@@ -8,10 +13,16 @@ export default class CatCard extends Component {
 
     render() {
         return (
-            <div className="CatCard"> 
-            	<div>{this.props.name}</div>
-
-            </div>
+            	<Card className="CatCard" outline>
+			        <CardImg top width="100%" 
+			        src={this.props.img || "https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"} alt="Card image cap" />
+			        <CardBody>
+			          <CardTitle>{this.props.name}</CardTitle>
+			          <CardSubtitle>Card subtitle</CardSubtitle>
+			          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+			          <CatPopUp buttonLabel="Info" />
+			        </CardBody>
+			    </Card>
         );
     }
 }
